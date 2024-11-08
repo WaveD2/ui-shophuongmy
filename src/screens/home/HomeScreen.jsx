922dbfe241e37485193b3f2f8d32c3a7358e8aad
dangtung <tungdev64@gmail.com>
2024-11-08 16:43:43 +0700

    update code

--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -6,10 +6,36 @@ import SavingZone from "../../components/home/SavingZone";
 import Catalog from "../../components/home/Catalog";
 import Brands from "../../components/home/Brands";
 import Feedback from "../../components/home/Feedback";
+import { useEffect, useState } from "react";
+import ENDPOINTS from "../../api/endpoins";
+import { apiClient } from "../../api/axios";
 
 const HomeScreenWrapper = styled.main``;
 
 const HomeScreen = () => {
+
+  // const [categories, setCategories] = useState([]);
+
+  // useEffect(() => {
+  //   window.scrollTo(0, 0);
+  //   async function fetchCategory() {
+  //     try {
+
+  //       const data = await apiClient.get(`${ENDPOINTS.CATEGORY}/?filter[level]=1`);
+
+  //       if (!data?.items.length) setCategories([]);
+
+  //       setCategories(data.items);
+
+  //     } catch (error) {
+  //       console.error('Error fetching categories:', error);
+  //     }
+  //   }
+  //   fetchCategory();
+  // }, []);
+
+  // console.log("categories", categories);
+
   return (
     <HomeScreenWrapper>
       <Hero />
